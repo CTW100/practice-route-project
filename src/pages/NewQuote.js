ce8198844762e4987d5f145edd8e684540cd9281
CTW100 <85342844+CTW100@users.noreply.github.com>
2021-11-13 20:25:51 +0900

    283
    Implementing Programmatic (Imperative) Navigation

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,8 +1,14 @@
+import { useHistory } from 'react-router-dom';
+
 import QuoteForm from '../components/quotes/QuoteForm';
 
 const NewQuote = () => {
+  const history = useHistory();
+
   const addQuoteHandler = (quoteData) => {
     console.log(quoteData);
+
+    history.pushState('/quotes');
   };
 
   return <QuoteForm onAddQuote={addQuoteHandler} />;
